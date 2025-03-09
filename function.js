ce2a7f00e3b8986c7f79b96e14b4f595dff52b04
MD.ABU NAYEM <rajnayem882@gmail.com>
2025-03-09 21:50:42 +0600

    All recap

--- function.js
+++ function.js
@@ -1,7 +1,7 @@
-function sum(num1=0,num2=1){
-const result = num1*num2;
-console.log(result);
-}
+// function sum(num1=0,num2=1){
+// const result = num1*num2;
+// console.log(result);
+// }
 
 
 // sum(10)
---
+++ recap.js
@@ -0,0 +1,53 @@
+// var
+
+// const and let 
+// const countryNAme="Bd";
+
+// let age=11;
+// age=12;
+
+// console.log(`Ami tomai valobasi
+//     ahdjhd
+//     ${age}`)
+
+// function sum(num1,num2){
+//     return num1+num2;
+// }
+
+// const sum2 =(num1,num2)=>num1+num2;
+// const sum3=(num1,num2)=>{
+//     return num1+num2;
+// }
+
+// const number=[1,3,4,5,6,7,7,7];
+
+// console.log(...number)
+
+// const person={
+//     name:"hena",
+//     age:19
+// }
+
+// const {name,age}=person;
+
+// console.log(name,age)
+
+
+// const frd =["Hero", "Alam","Abrar"];
+
+// const [first,second]=frd;
+
+// console.log(second)
+
+// const person={
+//     name:"hena",
+//     age:19
+// }
+
+// console.log(Object.keys(person))
+// console.log(Object.entries(person))
+// console.log(Object.values(person))
+
+// Object.seal(person)
+// Object.freeze(person)
+
