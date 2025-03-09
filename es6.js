7c0d21ed5a2afc56c6d23a6ee72d2274e89ec5e8
MD.ABU NAYEM <rajnayem882@gmail.com>
2025-03-09 11:09:38 +0600

    first commit

---
+++ es6.js
@@ -0,0 +1,28 @@
+// const countyName="Bangladesh";
+
+
+
+// let age=10;
+
+// age=11;
+// console.log(age);
+
+// const friend=["hena","hero","alom"];
+
+// for (let i = 0; i < friend.length; i++) {
+//     const element = friend[i];
+//     console.log(element);
+    
+// }
+
+// // friend.push("SRK");
+
+// // console.log(friend);
+
+
+// const person = {
+//     name:"Abrar Nayem",
+// }
+
+// person.name="Hjueshf";
+// console.log(person);
---
+++ string.js
@@ -0,0 +1,16 @@
+// const anthem="amar sonar bangla
+// ami to"
+
+// const anthem=`Amar Sonar Bangla 
+// ami tomai valobasi
+// chirodin tmr aksh tmr batash`;
+
+// console.log(anthem)
+
+const countryName = "Bangladesh";
+
+// const anthem="amar sonar" +countryName+ "ami tomar valobasi";
+
+const anthem=`Amar Sonar ${countryName} ami tomai valobasi`
+
+console.log(anthem)
